822df6dc61749bdcf4cee36b1333d8ae2abd65c9
SerBeluga <gregorio.lope1898@gmail.com>
2022-09-21 19:27:57 -0500

    version 0.3, got api callouts down, now getting pretty JSON

--- src/main/java/project/AssemblyAPIDemo.java
+++ src/main/java/project/AssemblyAPIDemo.java
@@ -2,32 +2,56 @@ package project;
 
 import java.net.URI;
 import io.github.cdimascio.dotenv.Dotenv;
-import java.net.http.HttpRequest; 
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandler;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import com.google.gson.Gson; 
 
 public class AssemblyAPIDemo {
 
+    private static String jsonOutput;
+
     public static void main(String[] args) throws Exception { 
 
+        String maximius = "https://github.com/SerBeluga/Personal-GitHub-Projects/blob/master/src/Audio%20Files/Maximus%20Decimus%20Meridius.mp3"; 
+        String DEATH = null; //TODO: Placeholder replace with theoden-plennenor-fields soundbyte
         String assembly = "https://api.assemblyai.com/v2/transcript";
         URI api = new URI(assembly); 
 
 
-        //Was added to protect my api key, to run on your system must make a custom .env file and declare there ex. MY_API_KEY=asdfklassdklf
+        //Was added to protect my api key, to run on your system must make a custom .env file and declare there. EX. MY_API_KEY=asdfklassdklf
         Dotenv envVar = Dotenv.configure()
             .directory("./src/main/java/project")
             .ignoreIfMalformed()
             .ignoreIfMissing()
             .load();
 
-        
-        //TO-DO: REMOVE. WAS FOR TESTING ONLY*********************************************
+        //TODO: REMOVE. WAS FOR TESTING ONLY*********************************************
         System.out.println(String.format(("KEY IS %s."), envVar.get("API_KEY")));
-            
 
-       /*  HttpRequest postReq = HttpRequest.newBuilder()
+        Transcript audioClip = new Transcript(); 
+        audioClip.setAudio_url(maximius);
+        Gson gson = new Gson(); 
+        String jsonMaximus = gson.toJson(audioClip); 
+
+        System.out.println("*******" + jsonMaximus);
+
+        HttpRequest postReq = HttpRequest.newBuilder()
             .uri(api)
             .header("Authorization", envVar.get("API_KEY"))
-            . */
+            .POST(BodyPublishers.ofString(jsonMaximus))
+            .build();
+
+        HttpClient client = HttpClient.newHttpClient(); 
+        HttpResponse<String> postResp = httpClient.send(postReq, BodyHandlers.ofString());
+        System.out.println(postResp.body());
+
+
 
     
     }
---
+++ src/main/java/project/Transcript.java
@@ -0,0 +1,25 @@
+package project;
+
+public class Transcript {
+
+    private String audio_url;
+    private String id; 
+
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAudio_url() {
+        return audio_url;
+    }
+
+    public void setAudio_url(String audio_url) {
+        this.audio_url = audio_url;
+    } 
+    
+}
