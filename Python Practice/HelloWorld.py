1edcb109758915e469409d844c4a58b81a8ff75d
SerBeluga <gregorio.lope1898@gmail.com>
2023-04-30 23:42:19 -0500

    learning docker compose/mongoDB and learned some python

---
+++ Python Practice/HelloWorld.py
@@ -0,0 +1,22 @@
+
+welcomeMsg = "************Welcome to Goyo's Simple Addition Calculator. To use please input two numbers to add.**************"
+print(welcomeMsg.upper())
+num1 = float(input("Enter First Number: "))
+num2 = float(input("Enter Second Number: "))
+
+add = (num1, num2)
+result = sum(add)
+
+print("************SUM: " + str(result))
+
+
+welcomeMsgWeight = "********welcome to Goyo's weight converter bitch. to use type in your weight, followed by its unit of measurement.********"
+supportMsg = "Currently only supporting K(g) and (L)bs units, so type in k or l respectively"
+
+print(welcomeMsgWeight.upper())
+print(supportMsg.upper())
+
+weightInt = float(input("Please input your numerical weight: "))
+weightUnit = str(input("K(g) or (L)bs : "))
+
+if( weightUnit. == )
