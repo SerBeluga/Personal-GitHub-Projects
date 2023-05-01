0cf1f78f42e4f2ced177387483e083c761937636
SerBeluga <gregorio.lope1898@gmail.com>
2023-05-01 12:39:23 -0500

    trying to commit all updated files this time

---
+++ Docker_Compose/squire.js
@@ -0,0 +1,87 @@
+let squires = [
+    {
+      "email": "garrisonZamora@yggdrasil.net",
+      "gender": "M",
+      "country": "High Theocracy",
+      "lastname": "Zamora",
+      "firstname": "Garrison",
+      "totalSpentOnExp": 9.52,
+      "favoriteMartialArts": {
+        "1": "Taunt",
+        "2": "Gr. Taunt",
+        "3": "Dexterity Boost",
+        "4": "War Rally",
+        "5": "Rush",
+        "6": "Stampede"
+      },
+      "isSquireApprenticed": false
+    },
+    {
+      "email": "bettieGaines@yggdrasil.net",
+      "gender": "F",
+      "country": "Dragon Council State",
+      "lastname": "Gaines",
+      "firstname": "Bettie",
+      "totalSpentOnExp": 5.81,
+      "favoriteMartialArts": {
+        "1": "Fortification",
+        "2": "Gr. Fortification",
+        "3": "Dexterity Boost",
+        "4": "War Rally",
+        "5": "Rush",
+        "6": "Duo Slash"
+      },
+      "isSquireApprenticed": false
+    },
+    {
+      "email": "pollyBush@yggdrasil.net",
+      "gender": "F",
+      "country": "High Theocracy",
+      "lastname": "Bush",
+      "firstname": "Polly",
+      "totalSpentOnExp": 2.49,
+      "favoriteMartialArts": {
+        "1": "Taunt",
+        "2": "Gr. Taunt",
+        "3": "War Cry",
+        "4": "War Rally",
+        "5": "Rush",
+        "6": "Stampede"
+      },
+      "isSquireApprenticed": true
+    },
+    {
+      "email": "taylorShepard@yggdrasil.net",
+      "gender": "M",
+      "country": "Dragon Council State",
+      "lastname": "Shepard",
+      "firstname": "Taylor",
+      "totalSpentOnExp": 7.53,
+      "favoriteMartialArts": {
+        "1": "Fortification",
+        "2": "Gr. Taunt",
+        "3": "Dexterity Boost",
+        "4": "Gr. Dexterity Boost",
+        "5": "Slash",
+        "6": "Duo Slash"
+      },
+      "isSquireApprenticed": true
+    },
+    {
+      "email": "concettaTrujillo@yggdrasil.net",
+      "gender": "M",
+      "country": "Khanish States",
+      "lastname": "Trujillo",
+      "firstname": "Concetta",
+      "totalSpentOnExp": 3.29,
+      "favoriteMartialArts": {
+        "1": "Fortification",
+        "2": "Gr. Taunt",
+        "3": "War Cry",
+        "4": "War Rally",
+        "5": "Rush",
+        "6": "Stampede"
+      },
+      "isSquireApprenticed": false
+    }
+  ]
