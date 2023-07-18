70cc3d4f8d769bc4ce7aad64f7fff01cd97fa6de
SerBeluga <gregorio.lope1898@gmail.com>
2023-07-18 18:15:53 -0500

    updated because it wasnt compiling

--- assembly_api_demo/src/main/java/project/AssemblyAPIDemo.java
+++ assembly_api_demo/src/main/java/project/AssemblyAPIDemo.java
@@ -1,8 +1,8 @@
 package project;
 
-import java.net.URI;
 import io.github.cdimascio.dotenv.Dotenv;
 
+import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -21,24 +21,19 @@ public class AssemblyAPIDemo {
 
     public static void main(String[] args) throws Exception {
 
-        String maximius = "https://bit.ly/GladiatorAssembly"; //added bit.lys to make links more manageable, really long before
-        String deathTheoden = "https://bit.ly/TheodenAssembly";
-        String speakItalian = "https://bit.ly/SpeakItalianAssembly";
+        String maximius = "https://bit.ly/MaximusAssembly"; //added bit.lys to make links more manageable, really long before
+        String deathTheoden = "https://bit.ly/KingTheodenAssembly";
+        String speakItalian = "https://bit.ly/KingdomOfHeavenAssembly";
         final String ASSEMBLY = "https://api.assemblyai.com/v2/transcript/";
         final URI API = new URI(ASSEMBLY);
         final Gson mainGson = new Gson();
 
-
         // Was added to protect my api key, to run on your system must make a custom
         // .env file and declare there. EX. MY_API_KEY=asdfklassdklf
+        //figured this was a more industry standard way of doing this rather than making something like a constants file
         Dotenv env = Dotenv.configure()
-                .directory("./src/main/java/project")
-                .ignoreIfMalformed()
-                .ignoreIfMissing()
-                .load();
-
-        //TODO:REMOVE WAS FOR TESTING
-        logs.debug(String.format(("KEY IS %s."), env.get("API_KEY")));
+            .directory("./assembly_api_demo/src/main/java/project")
+            .load();
 
         Transcript gladiator = new Transcript(maximius, true, true); //adds punctuation from AssemblyAPI, adds auto text formatting from AssemblyAPI
         String jsonMaximus = mainGson.toJson(gladiator);
@@ -52,16 +47,17 @@ public class AssemblyAPIDemo {
         String testMaximus = String.format("**** TEST MAXIMUS %s", jsonMaximus); 
         String testDEATH = String.format("**** TEST THEODEN %s", jsonDeath);
         String testKog = String.format("**** TEST WHERE THE MEN SPEAK ITALIAN %s", jsonSpeakItalian); 
+
         //TODO: Everthing clears for now on the json front, lets see if the api will consume my transcript object
         logs.debug(testMaximus);
         logs.debug(testDEATH);
         logs.debug(testKog);
 
-        HttpRequest postReqMaximus = HttpRequest.newBuilder()
-                .uri(API)
-                .header("Authorization", env.get("API_KEY")) //env.get looks in your .env file for a variable declared in double brackets
-                .POST(BodyPublishers.ofString(jsonMaximus))
-                .build();
+         HttpRequest postReqMaximus = HttpRequest.newBuilder()
+            .uri(API)
+            .header("Authorization", env.get("API_KEY")) //env.get looks in your .env file for a variable declared there under a specific var name
+            .POST(BodyPublishers.ofString(jsonMaximus))
+            .build();
 
         HttpClient client = HttpClient.newHttpClient();
         HttpResponse<String> postResp = client.send(postReqMaximus, BodyHandlers.ofString());
